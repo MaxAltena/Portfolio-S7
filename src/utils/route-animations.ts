e8623815835cc1dc6fef89ac604dbbffda1cb675
Max Altena <max@maxaltena.com>
2020-10-28 02:42:49 +0100

    💄 Small UI changes

--- src/utils/route-animations.ts
+++ src/utils/route-animations.ts
@@ -2,7 +2,7 @@ import { trigger, transition, style, query, animate } from '@angular/animations'
 
 export const fadeAnimation = trigger('routeAnimations', [
 	transition('* <=> *', [
-		query(':enter', [style({ opacity: 0 })], { optional: true }),
+		query(':enter', [style({ opacity: 1 })], { optional: true }),
 		query(
 			':leave',
 			[style({ position: 'absolute', opacity: 1 }), animate('200ms ease-in-out', style({ opacity: 0 }))],
